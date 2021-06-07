80621326fa91d6e5db7a6da19fe1dba0ff136337
Aurélien Beveraggi <65907063+Aurelien-Be@users.noreply.github.com>
2021-06-07 17:10:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for, request, redirect
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+  
+@app.route('/submit', methods=['POST','GET'])
+def submit():
+  if request.method == 'POST':
+        ytlink = request.form['yt link']
+        lang = request.form['lang']
+        Transcriptions = YouTubeTranscriptApi.get_transcript(ytlink, languages=[lang])
+        formatter = TextFormatter()
+        text_formatted = formatter.format_transcript(Transcriptions)
+        return text_formatted
